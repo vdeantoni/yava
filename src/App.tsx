72f2ac6986cc86780a7d34fa5b8ecdf031326620
Vinicius De Antoni <vdeantoni@gmail.com>
2024-08-30 11:39:27 -0700

    Added Sentry

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,20 @@ import { Button } from "@/components/ui/button.tsx";
 import { Analytics } from "@vercel/analytics/react";
 import MadeBy from "@/components/MadeBy.tsx";
 import { isMobile } from "@/lib/utils.ts";
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://7ecb9144de14ea899c8af041bdb68045@o428318.ingest.us.sentry.io/4507867732377600",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 function App() {
   const { file, reset, video } = useAppStore();
